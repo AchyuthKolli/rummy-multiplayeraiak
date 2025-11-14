a0928e65a8b623204414049510094fbf1e6b1b84
Achyuth Kolli <112481840+AchyuthKolli@users.noreply.github.com>
2025-11-15 03:26:55 +0530

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import Profile from "./pages/Profile";
+
+<Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/CreateTable" element={<CreateTable />} />
+  <Route path="/Table" element={<Table />} />
+  <Route path="/profile" element={<Profile />} />
+</Routes>
