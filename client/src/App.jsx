1b645630356b79549b18ca722494a535cdcee763
Achyuth Kolli <112481840+AchyuthKolli@users.noreply.github.com>
2025-11-18 02:29:50 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import Home from "./pages/Home";
+import CreateTable from "./pages/CreateTable";
+import Table from "./pages/Table";
 import Profile from "./pages/Profile";
 
-<Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/CreateTable" element={<CreateTable />} />
-  <Route path="/Table" element={<Table />} />
-  <Route path="/profile" element={<Profile />} />
-</Routes>
+export default function App() {
+  const navigate = useNavigate();
+
+  // 🔥 SPA Navigation Bridge for Akadoodle Home
+  useEffect(() => {
+    window.__AKADOODLE_NAVIGATE = (to) => navigate(to);
+  }, [navigate]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/CreateTable" element={<CreateTable />} />
+      <Route path="/Table" element={<Table />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
+}
